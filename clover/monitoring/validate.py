9055271aa74c002e5bf8dd88d18a55970789b650
QiLiang <liangqi1@huawei.com>
2018-03-15 12:20:59 +0800

    Initial commit for monitoring by prometheus
    - install prometheus
    - validate the installation
    - TODO: test collecting telemetry data from istio
    
    JIRA: CLOVER-7
    
    Change-Id: I983be2db78c8c5c20c0acee9ae81e891884e07fb
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ clover/monitoring/validate.py
@@ -0,0 +1,57 @@
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import requests
+
+PROMETHEUS_URL = "http://127.0.0.1:9090"
+PROMETHEUS_HEALTH_UP = "up"
+PROMETHEUS_ISTIO_TARGETS = {"envoy",
+    "istio-mesh",
+    "kubernetes-apiservers",
+    "kubernetes-cadvisor",
+    "kubernetes-nodes",
+    "kubernetes-service-endpoints",
+    "mixer",
+    "pilot"}
+
+def is_targets_healthy(url, istio_targets):
+    targets = set()
+    try:
+        # https://prometheus.io/docs/prometheus/latest/querying/api/#targets
+        response = requests.get('{0}/api/v1/targets'.format(url))
+        if response.status_code != 200:
+            print("ERROR: get targets status code: %r" % response.status_code)
+            return False
+    except Exception as e:
+        print("ERROR: Cannot connect to prometheus")
+        print(e)
+        return False
+
+    results = response.json()
+    for target in results["data"]["activeTargets"]:
+        if target["health"] != PROMETHEUS_HEALTH_UP:
+            print("ERROR: target unhealth job: %s, health: %s" % \
+                (target["labels"]["job"], target["health"]))
+            return False
+        targets.add(target["labels"]["job"])
+
+    diff = istio_targets - targets
+    if len(diff):
+        print("ERROR: targets %r not found!" % diff)
+        return False
+
+    return True
+
+def main():
+    if not is_targets_healthy(PROMETHEUS_URL, PROMETHEUS_ISTIO_TARGETS):
+        print("ERROR: Prometheus targets is unhealthy!")
+    else:
+        print("Prometheus targets are all healthy!")
+
+if __name__ == '__main__':
+    main()
+
