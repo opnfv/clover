1575a9b3f7e7f475d4d5d8d1541f783948c0d398
Eddie Arrage <eddie.arrage@huawei.com>
2018-03-21 18:38:59 +0000

    Added initial nginx services
    - Proxy allows ingress traffic to be sent to another element in
    service mesh
    - Mirroring is also in the default configuration
    - Default configuration is to proxy to a clover-server and mirror
    to snort-ids
    - A location_path (URI in HTTP requests) can be reconfigured to
    restrict proxing; default to '/'
    - A proxy_path can be reconfigured to specify an alternate destination
    - A mirror path can be reconfigured to specify where traffic
    will be spanned
    - The default server_port (listen port) for the proxy is 9180 but can be
    reconfigured
    - The default server_name is http-proxy but can be reconfigured
    - Reconfiguration is done over GRPC with jinja2 template for nginx
    - Currently snort ids sends alerts to proxy with stub code in GRPC
    
    - Refactored the code to have a nginx base with subservices
    - Proxy, Load Balancer (lb), and Server can share code - mainly GRPC
    server
    - Nginx subservices have separate docker builds
    
    - Improved build scripts for CI
    - Render yaml manifests dynamically
    - Improve nginx_client for runtime modifications (but not really
    useful yet)
    
    Change-Id: Icbff6890021bcc8a8da4690c9261205d6e1ca43a
    Signed-off-by: Eddie Arrage <eddie.arrage@huawei.com>

---
+++ samples/services/nginx/docker/process/grpc_process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+python grpc/nginx_grpc_server.py $1
+
