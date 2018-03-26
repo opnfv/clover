96ba1d0efd55442ddb36c8d899bd3fa5bfd11751
QiLiang <liangqi1@huawei.com>
2018-03-26 15:46:07 +0800

    [WIP]Add wrapper scripts for sample services
    - build.sh for building and pushing docker images
    - gen-kube-config.sh for generating kubernetes configureation file
    
    Dependency:
        https://gerrit.opnfv.org/gerrit/#/c/54225/
        https://gerrit.opnfv.org/gerrit/#/c/54269/
    
    Change-Id: Ifb2d1e4be36642ca616795a75326c9212e82578b
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ samples/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+set -ex
+
+CLOVER_BASE_DIR=`cd ${BASH_SOURCE[0]%/*}/..;pwd`
+export IMAGE_PATH=opnfv
+
+
+cd $CLOVER_BASE_DIR/samples/services/nginx/docker/
+
+./build_lb.sh
+./build_proxy.sh
+./build_server.sh
+
+cd $CLOVER_BASE_DIR/samples/services/snort_ids/docker/
+./build
+
+
+echo "Clover sample build complete!"
