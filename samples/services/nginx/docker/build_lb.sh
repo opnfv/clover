e1d8326262c15eba74ec68c40ad9285d56120bbe
QiLiang <liangqi1@huawei.com>
2018-03-26 15:46:07 +0800

    [WIP]Add wrapper scripts for sample services
    - build.sh for building and pushing docker images
    - [TODO] update gen-kube-config.sh for generating kubernetes configureation file
    
    Change-Id: Ifb2d1e4be36642ca616795a75326c9212e82578b
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- samples/services/nginx/docker/build_lb.sh
+++ samples/services/nginx/docker/build_lb.sh
@@ -11,6 +11,14 @@
 IMAGE_PATH=${IMAGE_PATH:-"localhost:5000"}
 IMAGE_NAME=${IMAGE_NAME:-"clover-ns-nginx-lb"}
 
-docker build -f subservices/lb/Dockerfile -t $IMAGE_NAME .
-docker tag $IMAGE_NAME $IMAGE_PATH/$IMAGE_NAME
+docker build -f subservices/lb/Dockerfile -t $IMAGE_PATH/$IMAGE_NAME .
+#docker tag $IMAGE_NAME $IMAGE_PATH/$IMAGE_NAME
+whoami
+pwd
+ls ~/.docker
+cat ~/.docker/config.json
+cd ~/
+pwd
+cd -
+docker images
 docker push $IMAGE_PATH/$IMAGE_NAME
