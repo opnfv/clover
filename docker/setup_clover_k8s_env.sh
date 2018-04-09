a5e5b2f2eaf50f2cbec43d4ecd2a4050ea5ee2af
QiLiang <liangqi1@huawei.com>
2018-04-09 02:33:25 -0700

    [WIP] Run overall clover env setup and validation
    - test.sh
        Run test in opnfv/clover container
    - setup_clover_k8s_env.sh
        install logging, monitoring and tracing tool
        wait all tool pods ready
    - validate_clover_k8s_env.sh
        setup port forwarding and run basic validation
        TODO: validate rr
    
    Change-Id: Iff9c768a829fb1ee76baede11dfa2563b92deb52
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ docker/setup_clover_k8s_env.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+set -ex
+
+CLOVER_BASE_DIR=$(cd ${BASH_SOURCE[0]%/*}/..;pwd)
+
+ISTIO_NAMESPACE="istio-system"
+REDIS_NAMESPACE="default"
+LOGGING_NAMESPACE="logging"
+MONITORING_NAMESPACE=$ISTIO_NAMESPACE
+TRACING_NAMESPACE=$ISTIO_NAMESPACE
+
+LOGGING_PODS_PREFIX="elasticsearch,fluentd-es,kibana"
+MONITORING_PODS_PREFIX="prometheus"
+REDIS_PODS_PREFIX="redis"
+TRACING_PODS_PREFIX="jaeger-deployment"
+
+cd $CLOVER_BASE_DIR
+pip install --upgrade ./
+
+kubectl apply -f ./clover/logging/install
+clover/tools/wait_for_pods.py $LOGGING_NAMESPACE $LOGGING_PODS_PREFIX running
+
+kubectl apply -f /usr/local/istio-source/install/kubernetes/addons/prometheus.yaml
+clover/tools/wait_for_pods.py $MONITORING_NAMESPACE $MONITORING_PODS_PREFIX running
+
+kubectl apply -f ./clover/tools/yaml/redis.yaml
+clover/tools/wait_for_pods.py $REDIS_NAMESPACE $REDIS_PODS_PREFIX running
+
+kubectl apply -n istio-system -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml
+clover/tools/wait_for_pods.py $TRACING_NAMESPACE $TRACING_PODS_PREFIX running
+
+echo "Setup Clover Kubernetes environment complete!"
