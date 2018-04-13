f3c049d63d52b14fdce3d24ebfa1fb155540a8ac
Eddie Arrage <eddie.arrage@huawei.com>
2018-04-13 19:15:29 +0000

    Add SDC deploy/clean scripts
    - Added deploy/clean scripts for use in Clover container
    
    Change-Id: Id9548a77d71465a814a6e0cb1cbdf02d37235590
    Signed-off-by: Eddie Arrage <eddie.arrage@huawei.com>

---
+++ samples/scenarios/deploy.sh
@@ -0,0 +1,22 @@
+/bin/bash
+#
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+CLOVER_BASE_DIR=${CLOVER_BASE_DIR:-"/home/opnfv/repos/clover"}
+ISTIO_BASE_DIR=${ISTIO_BASE_DIR:-"/istio-source"}
+
+cd $CLOVER_BASE_DIR
+
+echo "Deploying Istio manual sidecar injection without TLS authentication"
+
+kubectl apply -f $ISTIO_BASE_DIR/install/kubernetes/istio.yaml
+
+echo "Deploying Service Delivery Controller sample scenario"
+
+kubectl apply -f <(istioctl kube-inject --debug -f ./samples/scenarios/service_delivery_controller_opnfv.yaml)
