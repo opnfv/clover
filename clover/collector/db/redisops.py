dbece18d19c3977019c6727fcbe7a436031666fe
Eddie Arrage <eddie.arrage@huawei.com>
2018-05-09 18:33:55 +0000

    Initial commit for Clover Collector
    - Added a container named clover-collector using clover
    container as a base with build script
    - GRPC server to manage collector process
    - Cassandra DB client interface to initialize visibility keyspace
    - Init messaging adds table schemas for tracing - traces & spans
    - Adds table for monitoring - metrics
    - Does not implement Cassandra server but developed using
    public Cassandra docker container
    - Collector process in simple loop that periodically fetches
    traces and monitoring data and inserts to Cassandra - not optimized
    for batch retrieval yet for monitoring
    - CLI interface added to collector process and used
    by GRPC server for configuration
    - Simple GRPC client script to test GRPC server and start/stop
    of collector process
    - Collector process can be configured with access for tracing,
    monitoring and Cassandra
    - Added a return value in monitoring query method
    
    - Added ability to truncate tracing, metrics and spans tables
    in cql
    - Added cql prepared statements and batch insert for metrics
    and spans
    - Align cql connection to cql deployment within k8s
    - Fix issue with cql host list using ast and collect process
    args with background argument
    - Added redis interface to accept service/metric list
    externally for monitoring (will work in conjunction
    with clover-controller)
    - Use k8s DNS names and default ports for monitoring, tracing
    and cassandra
    - Added yaml manifest renderer/template for collector
    
    Change-Id: I3e4353e28844c4ce9c185ff4638012b66c7fff67
    Signed-off-by: Eddie Arrage <eddie.arrage@huawei.com>

---
+++ clover/collector/db/redisops.py
@@ -0,0 +1,59 @@
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import redis
+import logging
+
+REDIS_HOST = 'redis'
+# REDIS_HOST = '10.244.0.85'
+
+
+class RedisOps:
+
+    def __init__(self, host=REDIS_HOST):
+        logging.basicConfig(filename='redisops.log',
+                            level=logging.DEBUG)
+        try:
+            self.r = redis.StrictRedis(host=host, port=6379, db=0)
+        except Exception as e:
+            logging.debug(e)
+
+    def init_services(self, skey='visibility_services'):
+        service_names = ['http_lb', 'proxy_access_control']
+        for s in service_names:
+            self.r.sadd(skey, s)
+
+    def init_metrics(self, pkey='metric_prefixes', skey='metric_suffixes'):
+        metric_prefixes = ['envoy_cluster_out_', 'envoy_cluster_in_']
+        metric_suffixes = [
+            '_default_svc_cluster_local_http_internal_upstream_rq_2xx',
+            '_default_svc_cluster_local_http_upstream_cx_active']
+        for p in metric_prefixes:
+            self.r.sadd(pkey, p)
+        for s in metric_suffixes:
+            self.r.sadd(skey, s)
+
+    def get_services(self, skey='visibility_services'):
+        services = self.r.smembers(skey)
+        return services
+
+    def get_metrics(self, pkey='metric_prefixes', skey='metric_suffixes'):
+        prefixes = self.r.smembers(pkey)
+        suffixes = self.r.smembers(skey)
+        return prefixes, suffixes
+
+
+def main():
+    r = RedisOps()
+    r.init_services()
+    r.init_metrics()
+    r.get_services()
+    r.get_metrics()
+
+
+if __name__ == '__main__':
+    main()
