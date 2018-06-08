80ca68a6c9853290ea222420f4d66495d5a89148
wutianwei <wutianwei1@huawei.com>
2018-06-08 09:49:36 +0800

    [clover] Add script to setup k8s
    what the script do
    1. git clone the XCI repo
    2. set variable to deploy k8s
    
    how to use
    run the below command in you teminal
    ./xci-k8s-setup.sh
    
    requirement
    1. don't under the root user
    2. run sudo without password
    
    Change-Id: I5015e12d8d3b0db31285d5e817b3c40d6739ba22
    Signed-off-by: wutianwei <wutianwei1@huawei.com>

---
+++ xci-k8s-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CLOVER_DIR=`cd ${BASH_SOURCE[0]%/*}/;pwd`
+export CLOVER_DIR
+
+# Set the variable for deploying k8s
+export XCI_FLAVOR=${XCI_FLAVOR:-mini}
+export INSTALLER_TYPE=${INSTALLER_TYPE:-kubespray}
+export DEPLOY_SCENARIO=${DEPLOY_SCENARIO:-k8-flannel-nofeature}
+
+WORK_DIR=${CLOVER_DIR}/work
+sudo rm -rf $WORK_DIR
+mkdir $WORK_DIR
+
+# If SSH key doesn't exist generate an SSH key in $HOME/.ssh/
+[[ ! -d "$HOME/.ssh/" ]] && mkdir $HOME/.ssh/
+[[ ! -f "$HOME/.ssh/id_rsa" ]] && ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N ""
+
+sudo apt-get update
+sudo apt-get install git python-pip -y
+
+git clone https://gerrit.opnfv.org/gerrit/releng-xci $WORK_DIR/releng-xci
+
+cd $WORK_DIR/releng-xci/xci
+
+source xci-deploy.sh
+
+MASTER_IP=$(ssh root@$OPNFV_HOST_IP  "grep -r server ~/.kube/config | awk '{print \$2}' |awk -F '[:/]' '{print \$4}'")
+echo "----------------------------------------"
+echo "Info: You can login the Kubernetes Cluster master host"
+echo "ssh root@$MASTER_IP"
+echo "----------------------------------------"
