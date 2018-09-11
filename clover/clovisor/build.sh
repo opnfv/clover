2bb20ecc752a7be068fd25ebd32ede4e9852333b
Stephen Wong <stephen.kf.wong@gmail.com>
2018-09-11 23:37:33 +0000

    (WIP) CLOVER-43: Initial commit for Clovisor
    Part of Clover's initial task (during project proposal) is to explore
    cloud native network tracing tools. Clover the project proposes
    Clovisor as a way for Clover to use the IOvisor
    (https://www.iovisor.org/) to perform network tracing. This is the first
    commit of this module
    
    Please note that the BPF code used in this commit were only tested with
    Linux kernel 4.14+ --- hence, if ubuntu is used, and one prefers LTS,
    the requirement is 18.04+
    
    A different patch will track the redis and k8s API server watcher code
    
    Also, a design doc will be added in the future (TBD)
    
    Signed-off-by: Stephen Wong <stephen.kf.wong@gmail.com>
    Change-Id: I30d9f9d474b8703097c470d39628e86bc788f9b6

---
+++ clover/clovisor/build.sh
@@ -0,0 +1,45 @@
+# Copyright (c) Authors of Clover
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+GOVERSION=1.10.3
+OS=linux
+ARCH=amd64
+GOPATH=/home/ubuntu/go
+CLIENTGOVERSION=v8.0.0
+
+SRCDIR=`pwd`
+
+wget https://dl.google.com/go/go$GOVERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzf go$GOVERSION.$OS-$ARCH.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$GOPATH/bin:$PATH
+
+sudo apt install -y gcc
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D4284CDD
+echo "deb https://repo.iovisor.org/apt/bionic bionic main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt-get update -y
+sudo apt-get install -y bcc-tools libbcc-examples linux-headers-$(uname -r)
+
+go get github.com/google/gopacket
+go get github.com/iovisor/gobpf
+go get github.com/opentracing/opentracing-go
+go get github.com/pkg/errors
+go get github.com/uber/jaeger-client-go
+go get github.com/vishvananda/netlink
+go get github.com/vishvananda/netns
+go get golang.org/x/sys/unix
+
+go get github.com/tools/godep
+go get k8s.io/client-go/...
+cd $GOPATH/src/k8s.io/client-go
+git checkout $CLIENTGOVERSION
+godep restore ./...
+
+cd $SRCDIR/libclovisor
+go build .
+cd ../
+go build -o clovisor .
---
+++ clover/clovisor/clovisor_main.go
@@ -0,0 +1,58 @@
+// Copyright (c) Authors of Clover
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Apache License, Version 2.0
+// which accompanies this distribution, and is available at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
+
+    clovisor "./libclovisor"
+)
+
+var podMonitoringMap map[string]*clovisor.ClovisorBCC
+
+func main() {
+    node_name := os.Getenv("MY_NODE_NAME")
+
+    podMonitoringMap = make(map[string]*clovisor.ClovisorBCC)
+
+    clovisor_k8s_client, err := clovisor.K8s_client_init(node_name)
+    if err != nil {
+        fmt.Printf("Clovisor to Kubernetes connectivity failed: %v\n", err)
+        return
+    }
+    fmt.Printf("Clovisor got k8s client succeed\n")
+
+    monitoring_info_map, err := clovisor_k8s_client.Get_monitoring_info(node_name)
+    if err != nil {
+        fmt.Printf("Clovisor getting monitoring info failed: %v\n", err)
+        return
+    }
+    fmt.Printf("Clovisor get monitoring info succeed: %v\n", monitoring_info_map)
+
+    for pod := range monitoring_info_map {
+        podMon, err := clovisor.ClovisorNewPodInit(clovisor_k8s_client, pod,
+                                                   monitoring_info_map[pod])
+        if err != nil {
+            fmt.Printf("Clovisor monitoring pod %s failed: %v\n", pod, err)
+            continue
+        }
+        podMonitoringMap[pod] = podMon
+    }
+
+    sig := make(chan os.Signal, 1)
+    signal.Notify(sig, os.Interrupt, os.Kill, syscall.SIGTERM)
+    <-sig
+    for pod := range podMonitoringMap {
+        fmt.Printf("Send stop pod to pod %v\n", pod)
+        podMonitoringMap[pod].StopPod()
+    }
+}
+
