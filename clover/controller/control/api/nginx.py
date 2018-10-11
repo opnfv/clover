b850f07e5abce7545243eeb674495b6960d6d105
earrage <eddie.arrage@huawei.com>
2018-10-11 14:11:31 -0700

    Add nginx services control functionality
    - Modify nginx server template to use clover-controller
    in clover-system namespace for file upload
    - Add ability to set nginx server paths and move uploaded files
    in clover-controller and set/get file upload metadata
    - Add cloverctl commands to set nginx server and lb configurations
    - Add example yaml files to configure nginx server and
    lb v1/v2 from cloverctl
    - Modify service definition for http-lb versions to distinguish
    the two versions for GRPC messaging from clover-controller in SDC
    deployment yaml
    
    Change-Id: I5c6866c5ff3de358939c58ea8c0bde64a69c6eca
    Signed-off-by: earrage <eddie.arrage@huawei.com>

--- clover/controller/control/api/nginx.py
+++ clover/controller/control/api/nginx.py
@@ -15,34 +15,65 @@ import logging
 nginx = Blueprint('nginx', __name__)
 
 
-@nginx.route("/nginx/slb", methods=['GET', 'POST'])
-def slblist():
+@nginx.route("/nginx/lb", methods=['GET', 'POST'])
+def modify_lb():
     grpc_port = '50054'
     try:
         p = request.json
         try:
-            slb_name = p['slb_name']
-            nginx_grpc = slb_name + ':' + grpc_port
+            lb_name = p['lb_name']
+            nginx_grpc = lb_name + ':' + grpc_port
             channel = grpc.insecure_channel(nginx_grpc)
             stub = nginx_pb2_grpc.ControllerStub(channel)
 
             s_list = []
-            for s in p['slb_list']:
+            for s in p['lb_list']:
                 s_list.append(s['url'])
-            slb_list = pickle.dumps(s_list)
+            lb_list = pickle.dumps(s_list)
             response = stub.ModifyLB(nginx_pb2.ConfigLB(
                 server_port=p['server_port'], server_name=p['server_name'],
-                slb_list=slb_list,
-                slb_group=p['slb_group'], lb_path=p['lb_path']))
+                slb_list=lb_list,
+                slb_group=p['lb_group'], lb_path=p['lb_path']))
         except (KeyError, ValueError) as e:
             logging.debug(e)
-            return Response('Invalid value in test plan json/yaml', status=400)
+            return Response('Invalid value in LB json/yaml', status=400)
     except Exception as e:
         logging.debug(e)
         if e.__class__.__name__ == "_Rendezvous":
             return Response("Error connecting to LB via gRPC", status=400)
         else:
             return Response("Error modifying LB server list", status=400)
+    return response.message
+
+
+@nginx.route("/nginx/server", methods=['GET', 'POST'])
+def modify_server():
+    grpc_port = '50054'
+    try:
+        p = request.json
+        try:
+            server_name = p['server_name']
+            nginx_grpc = server_name + ':' + grpc_port
+            channel = grpc.insecure_channel(nginx_grpc)
+            stub = nginx_pb2_grpc.ControllerStub(channel)
+
+            locations = pickle.dumps(p['locations'])
+            files = pickle.dumps(p['files'])
+            response = stub.ModifyServer(nginx_pb2.ConfigServer(
+                server_port=p['server_port'], server_name=p['server_name'],
+                site_root=p['site_root'], locations=locations, files=files,
+                site_index=p['site_index'],
+                upload_path_config=p['upload_path_config'],
+                upload_path_test=p['upload_path_test']))
+        except (KeyError, ValueError) as e:
+            logging.debug(e)
+            return Response('Invalid value in server json/yaml', status=400)
+    except Exception as e:
+        logging.debug(e)
+        if e.__class__.__name__ == "_Rendezvous":
+            return Response("Error connecting to server via gRPC", status=400)
+        else:
+            return Response("Error modifying server", status=400)
     return response.message
 
 
